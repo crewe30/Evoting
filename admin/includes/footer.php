a4666764ba91b6ffeeaf017f2821e0893d98b8a9
Rubahimbya <49554988+Rubahimbya@users.noreply.github.com>
2021-07-14 15:55:37 +0300

    my first

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 <a href="https://dmusoke@umu.ac.ug/">Uganda Martyrs University</a></strong>
+</footer>
